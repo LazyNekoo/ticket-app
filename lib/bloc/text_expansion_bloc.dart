3ae9562ced9ea7c85477eef38f5391d4408e87e6
LazyNekoo <luqmannaqiuddin17@gmail.com>
2025-05-11 10:29:52 +0800

    Complete the tutorial

---
+++ lib/bloc/text_expansion_bloc.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ticket_app/bloc/text_expansion_events.dart';
+import 'package:ticket_app/bloc/text_expansion_states.dart';
+
+class TextExpansionBlocs extends Bloc<TextExpansionEvents, TextExpansionStates>{
+  TextExpansionBlocs():super(IsExpandedState(false)){
+    on<IsExpandedEvent>((event, state){
+
+    });
+  }
+}
